41f5eca3b15cb3d31d6db1fb6513f37e2ef6283f
jecihjoy <jecihjoy@gmail.com>
2019-02-11 16:59:53 +0300

    app setup

---
+++ src/main/java/org/github/jess/currencyrates/currencyratesmicroservice/CurrencyratesMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package org.github.jess.currencyrates.currencyratesmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyratesMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyratesMicroserviceApplication.class, args);
+	}
+
+}
+
